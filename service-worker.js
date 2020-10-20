6c152f5cd7e5c472942b4a0e3d70709748d4dc6d
Yazheviks <vicsergeyeva@gmail.com>
2020-10-20 16:06:05 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56475be9cba24000b69e470ba1f5d46c.js"
+  "./precache-manifest.bb59365934a5d1049d326092edc5eda1.js"
 );
 
 self.addEventListener('message', (event) => {
